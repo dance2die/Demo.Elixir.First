de357ff2d114b5ff558f9d248db0d0def1de1ca5
Sung <sungmkim80@gmail.com>
2017-04-29 17:09:30 -0400

    default parameters

--- sample_enum.exs
+++ sample_enum.exs
@@ -1,5 +1,13 @@
 defmodule Sample.Enum do
     # def first([]), do: nil
-    def first(list) when length(list) == 0, do: nil
-    def first([head | _]), do: head
+    # def first(list) when length(list) == 0, do: nil
+    # def first([head | _]), do: head
+
+    def first(list, val \\ nil)
+    def first([head | _], _), do: head
+    def first([], val), do: val
+
+    def add(list, val \\ 0) do
+        [val | list]
+    end
 end
