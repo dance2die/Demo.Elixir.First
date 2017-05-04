cbdcec4506a27f66f9c7212a290e4d65c2fc34bd
Sung <sungmkim80@gmail.com>
2017-05-03 20:27:46 -0400

    added map2

--- sample_enum.exs
+++ sample_enum.exs
@@ -17,6 +17,18 @@ defmodule Sample.Enum do
         [f.(hd) | map(tl, f)]
     end
 
+    def reverse(l), do: reverse(l, [])
+    def reverse([], reversed), do: reversed
+    def reverse([head | tail], reversed),
+        do: reverse(tail, [head | reversed])
+
+    def map2([head | tail], f), 
+        do: map2(tail, f, [f.(head)])
+    def map2([], _, result), 
+         do: reverse(result)
+    def map2([head | tail], f, result), 
+        do: map2(tail, f, [f.(head) | result])
+
     defp trace(string) do
         IO.puts("the value passed in was #{string}")
     end
